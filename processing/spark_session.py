6da81f2871769912c0c5ec269187885d7ed16a51
Kanhaiya <kanhaiyaschaudhari@gmail.com>
2026-09-17 16:07:29 +0530

    spark session added

---
+++ processing/spark_session.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+
+def get_spark(app_name: str) -> SparkSession:
+  return(
+    SparkSession.builder
+    .appName(app_name)
+    .config("spark.jars.packages","io.delta:delta-spark_2.12:3.1.0")
+    .config("spark.sql.extensions",
+            "io.delta.sql.DeltaSparkSessionExtension")
+    .config("spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    .master("local[*]")
+    .getOrCreate()
+  )
